37f7610e054502ea6c9974525c8d3572095524e9
sam <mstitop@gmail.com>
2011-02-28 16:54:57 +0800

    test commit

--- src/com/esen/hibernate/Test.java
+++ src/com/esen/hibernate/Test.java
@@ -5,7 +5,9 @@ import org.hibernate.cfg.Configuration;
 
 public class Test {
 	public static void main(String[] args) {
+		//just for test
 		Configuration cfg = new Configuration();
 		SessionFactory sf = cfg.configure().buildSessionFactory();
+		
 	}
 }
