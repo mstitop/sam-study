c140f149043d5eb787f923f0c00cd70f98fb00eb
shiam <anming.shi@gmail.com>
2011-03-10 16:00:55 +0800

    add test

--- src/test/java/com/esen/hibernate/entity/UserTest.java
+++ src/test/java/com/esen/hibernate/entity/UserTest.java
@@ -1,6 +1,22 @@
 package com.esen.hibernate.entity;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.esen.hibernate.util.HibernateUtil;
 
 public class UserTest {
+	private static Logger log = LoggerFactory.getLogger(UserTest.class);
 
+	@Test
+	public void testAdd() {
+		log.debug("test");
+		User user = new User();
+		user.setName("test");
+		HibernateUtil.save(user);
+		assertNotNull(HibernateUtil.load(User.class, user.getId()));
+	}
 }
